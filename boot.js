885c3c4eb5630b57dad6e9fe95215df0fdfa170c
julio <jc_armenta99@hotmail.com>
2021-06-04 17:13:36 -0700

    test

---
+++ boot.js
@@ -0,0 +1,7 @@
+const leftBtn = document.querySelector('#left-btn')
+const rightBtn = document.querySelector('#right-btn')
+const slides = document.querySelectorAll('.carousel-item')
+
+leftBtn.addEventListener('click', () => {
+    
+})
